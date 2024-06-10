22a7d9e0d57356c5948df5702128da7e3f7956d2
carlos-rsalda <153567205+carlos-rsalda@users.noreply.github.com>
2024-06-10 15:20:47 -0600

    "Fix database URL scheme for PostgreSQL"

--- app/config.py
+++ app/config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///local.db')
+    db_uri = os.getenv('DATABASE_URL', 'sqlite:///local.db')
+    if db_uri.startswith('postgres://'):
+        db_uri = db_uri.replace('postgres://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URI = db_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
