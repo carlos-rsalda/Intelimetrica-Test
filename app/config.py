5cb63932d24e8743cfb6a849a8579df5bfd60732
carlos-rsalda <153567205+carlos-rsalda@users.noreply.github.com>
2024-06-10 15:14:48 -0600

    Configure application to use PostgreSQL database

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///restaurants.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///local.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
